c02e681fc7a7870694b7ef47bbb309b4fa864706
Hadrien Croubois <hc@iex.ec>
2019-08-25 01:00:25 +0200

    modules : trying to fix the network issue in torus

--- packages/modules/torus/lib/torus.js
+++ packages/modules/torus/lib/torus.js
@@ -3,5 +3,6 @@ const Torus = require("@toruslabs/torus-embed");
 window.provider = async (config) => {
 	const torus = new Torus.default();
 	await torus.init();
+	await torus.setProvider(config.provider.network);
 	return torus.provider;
 }
